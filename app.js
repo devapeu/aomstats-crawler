22955449e85704a1234559dc9ea31d1254e9755a
Piero del Valle <devapeu@gmail.com>
2025-08-02 19:38:10 -0400

    update url

--- app.js
+++ app.js
@@ -18,18 +18,21 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-async function crawlPlayerMatches(profileId) {
+async function crawlPlayerMatches(profileId, leaderboard = 0) {
   const allMatches = [];
-  const limit = 60;
+  //const limit = 60;
 
-  for (let start = 0; ; start += limit) {
-    const url = `https://aoe2.net/api/player/matches?game=aoe2de&profile_id=${profileId}&count=${limit}&start=${start}`;
+  const today = Math.floor(Date.now() / 1000);
+  const increment = 259200; // 3 days
+
+  for (let start = today; ; start += increment) {
+    const url = `https://aomstats.io/api/profile/${profileId}/matches?leaderboard=${leaderboard}&before=${start}`;
     const res = await axios.get(url);
     const matches = res.data;
 
     if (!matches.length) break;
     allMatches.push(...matches);
-    if (matches.length < limit) break;
+//    if (matches.length < limit) break;
     await new Promise(r => setTimeout(r, 500)); // throttle
   }
 

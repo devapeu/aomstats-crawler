19e90f61ceaad2c357cba63d9d70e003e46d3534
Piero del Valle <devapeu@gmail.com>
2025-08-05 09:36:48 -0400

    add limit

--- cron-job.js
+++ cron-job.js
@@ -14,8 +14,12 @@ const db = new Database('./db.sqlite');
   const seen = new Set();
   const allMatches = [];
 
+  const stmt = db.prepare(`SELECT MAX(startgametime) as latest FROM matches`);
+  const result = stmt.get();
+  const latestRecordDate = result.latest;
+
   for (const p of PLAYERS) {
-    const matches = await crawlPlayerMatches(p);
+    const matches = await crawlPlayerMatches(p, latestRecordDate);
     for (const m of matches) {
       const key = `${m.match_id}-${m.profile_id}`;
       if (!seen.has(key)) {

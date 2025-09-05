912fee928a78e96a96037da2324364c5c43617e5
Piero del Valle <devapeu@gmail.com>
2025-09-05 01:21:03 -0400

    add fix for db

---
+++ update_bad_matches.js
@@ -0,0 +1,17 @@
+// updateMatches.js
+const Database = require('better-sqlite3');
+
+// adjust the path to where your DB is inside the container
+const db = new Database('./db.sqlite');
+
+// Update rows from JSON
+
+const stmt = db.prepare(`
+    DELETE FROM matches
+    WHERE json_extract(raw_data, '$.resulttype') = 4
+       OR json_extract(raw_data, '$.duration') < 300
+  `);
+
+stmt.run();
+
+console.log('Update complete.');

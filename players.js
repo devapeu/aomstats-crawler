29de87c47aaa52e21245fb32216c4766778feeee
Piero del Valle <devapeu@gmail.com>
2025-10-22 13:25:09 -0400

    fix typo

--- players.js
+++ players.js
@@ -1,7 +1,7 @@
 // Add ID and name for players you want to track stats of.
 // Use the player's ID in aomstats.io as the key and define a name for them as the value.
 
- const PLAYERS = {
+const PLAYERS = {
   // Example
   // "1234556789": "HilariousAlpaca99"
 };

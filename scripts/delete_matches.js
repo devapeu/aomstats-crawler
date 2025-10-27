535e3480ae1a4d81a6ff083f1ec0113ce20ce49a
Piero del Valle <devapeu@gmail.com>
2025-10-27 19:11:26 -0400

    fix script

--- scripts/delete_matches.js
+++ scripts/delete_matches.js
@@ -5,12 +5,14 @@ const dbPath = path.resolve(__dirname, '..', 'db.sqlite');
 const db = new Database(dbPath);
 
 const stmt = db.prepare(`
-    DELETE FROM matches
-    WHERE match_id in 
-      (SELECT match_id FROM matches
-       WHERE json_extract(raw_data, '$.team') = 2
-    `);
+  DELETE FROM matches
+  WHERE match_id IN (
+    SELECT match_id
+    FROM matches
+    WHERE json_extract(raw_data, '$.team') = 2
+  )
+`);
 
 stmt.run();
 
-console.log('Update complete.');
+console.log('Delete complete.');

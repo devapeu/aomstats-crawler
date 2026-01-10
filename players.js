34741d94b69b67cf6581169da270ffea90be0143
Piero del Valle <devapeu@gmail.com>
2026-01-10 14:41:19 -0500

    clear players

--- players.js
+++ players.js
@@ -2,20 +2,8 @@
 // Use the player's ID in aomstats.io as the key and define a name for them as the value.
 
 const PLAYERS = {
-  '1074827715': "Ayax",
-  '1074199836': "Diego",
-  '1076671413': "Diego",
-  '1073862520': "Piero",
-  '1074875183': "Jair",
-  '1074196830': "Jaume",
-  '1074910820': "Sebastián",
-  '1075027222': "Renato",
-  '1074849746': "Héctor",
-  '1074203172': "Jardani",
-  '1074839111': "Christian",
-  '1075268390': "Almeyda",
-  '1074842985': "Jean Paul",
-  '1075636768': "André",
+  // Example
+  // "1234556789": "HilariousAlpaca99"
 };
 
 module.exports = PLAYERS;

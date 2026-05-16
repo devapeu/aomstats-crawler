947fcb19f8be0770eaf73c3683b4fb888dd0c785
Piero del Valle <devapeu@gmail.com>
2026-05-16 07:54:22 -0400

    update schema

--- database/index.js
+++ database/index.js
@@ -1,8 +1,8 @@
 const Database = require('better-sqlite3');
-const { insertMatches, deleteAsymmetricalMatches, computeAndUpdateTeamMatchIds } = require('../models/matches');
-const { updateEloForMatches, getPlayerElo } = require('../services/elo');
+const {insertMatches, deleteAsymmetricalMatches, computeAndUpdateTeamMatchIds} = require('../models/matches');
+const {updateEloForMatches, getPlayerElo} = require('../services/elo');
 const PLAYERS = require('../players');
-const { ELO_DEFAULT } = require('../config/eloConfig');
+const {ELO_DEFAULT} = require('../config/eloConfig');
 
 // Open or create DB
 const db = new Database('./db.sqlite');
@@ -10,45 +10,85 @@ const db = new Database('./db.sqlite');
 // Use player IDs (keys) to perform loops
 const playerIds = Object.keys(PLAYERS);
 
-// Create table (adjust columns as needed)
-db.exec(`
-  CREATE TABLE IF NOT EXISTS matches (
-    match_id INTEGER,
-    profile_id INTEGER,
-    description TEXT,
-    startgametime INTEGER,
-    raw_data TEXT,
-    win INTEGER,
-    team_match_id TEXT,
-    PRIMARY KEY(match_id, profile_id)
-  )
-`);
-
-db.exec(`
-  CREATE TABLE IF NOT EXISTS player_elo (
-    profile_id INTEGER PRIMARY KEY,
-    elo REAL DEFAULT ${ELO_DEFAULT},
-    last_updated INTEGER
-  )
-`);
-
 db.exec("PRAGMA foreign_keys = ON");
 
+// Create table (adjust columns as needed)
 db.exec(`
-  CREATE TABLE IF NOT EXISTS tournaments (
-    tournament_id INTEGER PRIMARY KEY,
-    name TEXT,
-    is_open INTEGER
-  )
+    CREATE TABLE IF NOT EXISTS matches
+    (
+        match_id          INTEGER PRIMARY KEY,
+        description       TEXT,
+        startgametime     INTEGER,
+        mapname           TEXT,
+        duration          INTEGER,
+        team_match_id     TEXT,
+        team_god_match_id TEXT
+    )
 `);
 
 db.exec(`
-  CREATE TABLE IF NOT EXISTS tournament_matches (
-    tournament_id INTEGER,
-    match_id INTEGER,
-    FOREIGN KEY (tournament_id) REFERENCES tournaments(tournament_id),
-    PRIMARY KEY (tournament_id, match_id)
-  )
+    CREATE TABLE IF NOT EXISTS players
+    (
+        profile_id INTEGER PRIMARY KEY,
+        name       TEXT
+    )`
+);
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS player_matches
+    (
+        match_id   INTEGER,
+        profile_id INTEGER,
+        god        TEXT,
+        win        INTEGER,
+        team       INTEGER,
+        PRIMARY KEY (match_id, profile_id),
+        FOREIGN KEY (match_id) REFERENCES matches(match_id),
+        FOREIGN KEY (profile_id) REFERENCES players(profile_id)
+        
+    )
+`);
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS player_elo
+    (
+        profile_id   INTEGER,
+        scope_type   TEXT,
+        scope_key    TEXT,
+        elo          REAL DEFAULT ${ELO_DEFAULT},
+        last_updated INTEGER,
+        PRIMARY KEY (profile_id, scope_type, scope_key),
+        FOREIGN KEY (profile_id) REFERENCES players(profile_id)
+    )
+`);
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS player_elo_meta
+    (
+        meta_key   TEXT,
+        meta_value TEXT,
+        scope      TEXT,
+        PRIMARY KEY (meta_key, meta_value, scope)
+    );
+`);
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS tournaments
+    (
+        tournament_id INTEGER PRIMARY KEY,
+        name          TEXT,
+        is_open       INTEGER
+    )
+`);
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS tournament_matches
+    (
+        tournament_id INTEGER,
+        match_id      INTEGER,
+        FOREIGN KEY (tournament_id) REFERENCES tournaments (tournament_id),
+        PRIMARY KEY (tournament_id, match_id)
+    )
 `)
 
 module.exports = {
@@ -58,5 +98,4 @@ module.exports = {
   deleteAsymmetricalMatches: () => deleteAsymmetricalMatches(db),
   computeAndUpdateTeamMatchIds: () => computeAndUpdateTeamMatchIds(db),
   updateEloForMatches: () => updateEloForMatches(db),
-  getPlayerElo: (profileId) => getPlayerElo(db, profileId),
 };

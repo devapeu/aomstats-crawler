ebff539b5d4cf66b1c885776d45e1e626948859f
Piero del Valle <devapeu@gmail.com>
2026-05-16 19:15:06 -0400

    update routes with services

--- routes/players.js
+++ routes/players.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { crawlPlayerMatches } = require('../services/aomstats');
-const { getStats } = require('../controllers/players');
-const { db, playerIds, insertMatches } = require('../database');
-const { getPlayerElo } = require('../models/elo');
+
+const { PlayerMatchesService } = require("../services/PlayerMatchesService");
+const { EloService } = require("../services/EloService");
 
 router.get('/fetch/:profileId', async (req, res) => {
   const { profileId } = req.params;
@@ -14,18 +14,12 @@ router.get('/fetch/:profileId', async (req, res) => {
 
 router.get('/gods/:profile_id', (req, res) => {
   const after = req.query.after ?? 0;
-  const rows = db.prepare(`
-    SELECT 
-      god,
-      COUNT(*) AS total_games,
-      ROUND(
-        COUNT(CASE WHEN win = 1 THEN 1 END) * 100.0 / COUNT(*),
-        2
-      ) AS winrate_percent
-    FROM matches
-    WHERE profile_id = ? AND startgametime > ?
-    GROUP BY god
-    ORDER BY total_games DESC`).all(req.params.profile_id, after);
+  const profileId = parseInt(req.params.profile_id);
+
+  const rows = PlayerMatchesService.getPlayerWinsByGod(
+    profileId,
+    { after }
+  );
 
   if (!rows.length) {
     return res.json({ god: null, message: 'No data found for this player' });
@@ -44,66 +38,93 @@ router.get('/gods/:profile_id', (req, res) => {
 
 router.get(
   '/partners/:profile_id',
-  getStats(db, playerIds, 'partners', req => req.params.profile_id)
+  (req, res) => {
+    const after = req.query.after ?? 0;
+    const god = req.query.god ?? null;
+    const profileId = parseInt(req.params.profile_id);
+
+    const rows = PlayerMatchesService.getPlayerWinsByTeammate(
+      profileId,
+      { god, after }
+    );
+
+    if (!rows.length) {
+      return res.json({ message: 'Unable to fetch data for this player' });
+    }
+
+    res.json({
+      players: rows,
+    });
+  }
 );
 
 router.get(
   '/rivals/:profile_id',
-  getStats(db, playerIds, 'rivals', req => req.params.profile_id)
+  (req, res) => {
+    const after = req.query.after ?? 0;
+    const god = req.query.god ?? null;
+    const profileId = parseInt(req.params.profile_id);
+
+    const rows = PlayerMatchesService.getPlayerWinsVsPlayers(
+      profileId,
+      { god, after }
+    );
+
+    if (!rows.length) {
+      return res.json({ message: 'Unable to fetch data for this player' });
+    }
+
+    res.json({
+      players: rows,
+    });
+  }
 );
 
 router.get('/winstreak/:profile_id', (req, res) => {
-  const query = db.prepare(`
-  WITH streaks AS (
-    SELECT
-      profile_id,
-      match_id,
-      win,
-      SUM(CASE WHEN win = 0 THEN 1 ELSE 0 END)
-        OVER (PARTITION BY profile_id ORDER BY match_id ROWS UNBOUNDED PRECEDING) AS loss_group
-    FROM matches
-    WHERE profile_id = ?
-  ),
-  grouped AS (
-    SELECT
-      profile_id,
-      loss_group,
-      COUNT(*) AS streak_length,
-      MAX(match_id) AS last_match_id
-    FROM streaks
-    WHERE win = 1
-    GROUP BY profile_id, loss_group
-  ),
-  last_match AS (
-    SELECT MAX(match_id) AS max_match_id
-    FROM matches
-    WHERE profile_id = ?
-  )
-  SELECT COALESCE(g.streak_length, 0) AS current_streak
-  FROM last_match lm
-  LEFT JOIN streaks s
-    ON s.match_id = lm.max_match_id
-  LEFT JOIN grouped g
-    ON g.profile_id = s.profile_id AND g.loss_group = s.loss_group;
-  `).all(req.params.profile_id, req.params.profile_id);
+  const profileId = parseInt(req.params.profile_id);
+  const winstreak = PlayerMatchesService.getPlayerWinstreak(profileId);
 
-  if (!query.length) {
+  if (!winstreak) {
     return res.json({ message: 'Unable to fetch data for this player' });
   }
 
   res.json({
-    winstreak: query[0].current_streak,
+    winstreak: winstreak
   });
 });
 
 router.get('/elo/:profile_id', (req, res) => {
   const profileId = req.params.profile_id;
-  const elo = getPlayerElo(db, profileId);
-  
+  const god = req.query.god ?? null;
+  const scope = god ? 'god' : 'global';
+
+  const elo = EloService.getElo(
+    profileId,
+    scope,
+    god,
+  );
+
   res.json({
     profile_id: profileId,
     elo: elo
   });
 });
+
+router.get('/elos/:profile_id', (req, res) => {
+  const profileId = parseInt(req.params.profile_id);
+
+  const rows = EloService.getAllElo(profileId);
+
+  if (!rows.length) {
+    return res.json({ message: 'No data found for this player' });
+  }
+
+  res.json({
+    elos: rows.map(row => ({
+      elo: row.elo,
+      god: row.god === '' ? null : row.god,
+    })),
+  })
+})
 
 module.exports = router;
